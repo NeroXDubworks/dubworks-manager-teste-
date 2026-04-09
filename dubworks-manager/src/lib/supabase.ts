50dbe8835826ff63168040c6c287ee9854039fec
NeroXDubworks <Lorrane.eugenio@outlook.com>
2026-04-09 19:02:49 -0300

    Delete dubworks-manager directory

--- dubworks-manager/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://omgjbafqukpzdhhpdlaa.supabase.co";
-const supabaseAnonKey = "sb_publishable_3bAOHbPjpV5RMnqb-cJKRA_cB1okqvT";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
